86bbd74c88136b4ddba6e6c3483efe349afcfb59
stux <coder@fbi.ac>
2023-05-18 13:14:10 +0200

    Added string and int variables in main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-#this is a test
+#n Variables
+firstname = "stux" # creating a string variable
+secondname = "rypt" # creating a second one
+fullname = firstname+" "+secondname #putting both variables together with a space between them
+print ("My full name is "+fullname) # prints my full name
+
+age = 16
+birthday = "no"
+if birthday == "yes":
+    age += 1
+
+
+print ("Your age is "+age)
+
